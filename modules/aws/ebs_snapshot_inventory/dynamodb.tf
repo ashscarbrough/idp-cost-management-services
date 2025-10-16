6f04c9128689932d5794daba168a0fcf83a23f46
Ash Scarbrough <ash.scarbrough@gmail.com>
2025-10-15 23:50:16 -0400

    fix var syntax

--- modules/aws/ebs_snapshot_inventory/dynamodb.tf
+++ modules/aws/ebs_snapshot_inventory/dynamodb.tf
@@ -1,6 +1,6 @@
 # #### EBS SNAPSHOT DDB TABLE #### #
 resource "aws_dynamodb_table" "ebs_snapshot_table" {
-  name         = "snapshot-deletion-schedule-${env}"
+  name         = "ebs-snapshot-deletion-schedule-${var.env}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ResourceId"
 
